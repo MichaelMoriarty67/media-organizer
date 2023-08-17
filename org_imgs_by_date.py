2a773bc20256f6b4653549c9e8b411bbaa8a121c
MichaelMoriarty67 <michaelmoriarty67@hotmail.com>
2023-08-17 13:04:36 -0700

    Error handling structure set

--- org_imgs_by_date.py
+++ org_imgs_by_date.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from datetime import datetime
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 from PIL.ExifTags import Base, TAGS
 from pathlib import Path
 
@@ -33,34 +33,23 @@ def file_date_from_os(file_path: str) -> str:
 def file_date_from_img(file_path: str) -> str:
     """Determine the file creation date for images."""
     creation_timestamp = None
-    try:
-        img = Image.open(file_path)
-        named_exif = _get_exif_names(img.getexif())
-        print(f"img's keys and vals: {named_exif}")
+    img = Image.open(file_path)
+    named_exif = _get_exif_names(img.getexif())
 
-        keys = ("DateTime", "DateTimeOriginal", "DateTimeDigitized")
-        vals = [named_exif[key] for key in keys if key in named_exif]
+    keys = ("DateTime", "DateTimeOriginal", "DateTimeDigitized")
+    vals = [named_exif[key] for key in keys if key in named_exif]
 
-        if len(vals) != 0:
-            for v in vals:
-                date = datetime.strptime(
-                    v, "%Y:%m:%d %H:%M:%S"
-                )  # convert value to datetime
-                if (
-                    creation_timestamp is None or date < creation_timestamp
-                ):  # compare to earliest date
-                    creation_timestamp = date
-
-    except Exception as e:
-        print(f"Error found in file_date_from_img: {e}")
-        # TODO: add analytics
+    if len(vals) != 0:
+        for v in vals:
+            date = datetime.strptime(
+                v, "%Y:%m:%d %H:%M:%S"
+            )  # convert value to datetime
+            if (
+                creation_timestamp is None or date < creation_timestamp
+            ):  # compare to earliest date
+                creation_timestamp = date
 
     return creation_timestamp
-
-
-def file_date_from_vid(file_path: str):
-    """Determine the file creation date for videos."""
-    pass
 
 
 def _get_exif_names(exif):
@@ -99,20 +88,10 @@ def _copy_binary_file(
 def determine_date(file_path: str) -> datetime:
     """Given a file, determines its creation date."""
     date = file_date_from_os(file_path)
+    img_date = file_date_from_img(file_path)
 
-    try:
-        img_date = file_date_from_img(file_path)
-        # TODO: raises when img_date is None when no exif tags exist
-
-        if img_date is not None and img_date < date:
-            date = img_date
-
-    except Exception:
-        print("determine_date message: \nThat ain't no img!")
-        vid_date = file_date_from_vid(file_path)
-
-        if img_date is not None and vid_date < date:
-            date = vid_date
+    if img_date is not None and img_date < date:
+        date = img_date
 
     return date
 
@@ -124,37 +103,34 @@ def _format_datetime(date: datetime) -> str:
 
 if __name__ == "__main__":
     print("WELCOME TO PICTURE ORGANIZER 7000.\n")
+
     try:
         source_path = Path(SOURCE_DIR)
         target_path = Path(TARGET_DIR)
 
-        # given dir, iterate through all files:
         for file in os.listdir(source_path):
-            # determine date for each file
-            source_file_path = source_path / file
-            print(f"file_path: {source_file_path}")
-            date = determine_date(source_file_path)
+            try:
+                source_file_path = source_path / file
+                date = determine_date(source_file_path)
 
-            # check if year folder exists, if not, create
-            year_path = target_path / str(date.year)
-            month_path = year_path / str(date.month)
+                year_path = target_path / str(date.year)
+                month_path = year_path / str(date.month)
 
-            if not year_path.is_dir():
-                # create year dir
-                year_path.mkdir()
-                # create month dir
-                month_path.mkdir()
+                if not year_path.is_dir():
+                    year_path.mkdir()
+                    month_path.mkdir()
 
-            if not month_path.is_dir():
-                # create month dir
-                month_path.mkdir()
+                if not month_path.is_dir():
+                    month_path.mkdir()
 
-            file_name = _format_datetime(date) + "." + _get_file_type(file)
-            breakpoint()
-            target_file_path = month_path / file_name
+                file_name = _format_datetime(date) + "." + _get_file_type(file)
+                target_file_path = month_path / file_name
 
-            # copy, move, rename each file
-            _copy_binary_file(source_file_path, target_file_path)
+                _copy_binary_file(source_file_path, target_file_path)
 
-    except TypeError and FileNotFoundError:
+            except UnidentifiedImageError:
+                print("yea that ain't an  type we know chief...")
+                # add one to analytic fails
+
+    except (TypeError, FileNotFoundError):
         print("Incorrect source or target path... please try again.")
